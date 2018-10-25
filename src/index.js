66e326541ca817c5fa343c1431f9fc9a8a19c76f
github username <github email address>
2018-10-25 19:31:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={store}> 
     <App store={store}/>
    </Provider>
   ,document.getElementById('root')
